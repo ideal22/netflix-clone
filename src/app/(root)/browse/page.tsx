59493bfe233a610a814bdc6265accfb8d041d6b8
Nodirbek <nodirbek.eshonkulov@smartbank.uz>
2024-01-08 17:53:48 +0500

    next auth

--- src/app/(root)/browse/page.tsx
+++ src/app/(root)/browse/page.tsx
@@ -2,11 +2,15 @@
 
 import Login from '@/components/shared/Login'
 import { useGlobalContext } from '@/context/GlobalContext'
+import { useSession } from 'next-auth/react'
 
 const BrowsePage = () => {
   const { account } = useGlobalContext()
+  const { data: session } = useSession()
 
-  if (account === null) return <Login />
+  console.log(session)
+
+  if (session === null) return <Login />
   return <div>BrowsePage</div>
 }
 export default BrowsePage
--- src/components/shared/Login.tsx
+++ src/components/shared/Login.tsx
@@ -1,4 +1,29 @@
+import Image from 'next/image'
+import { signIn } from 'next-auth/react'
+import { Button } from '../ui/button'
+import { FaGithub } from 'react-icons/fa'
+
 const Login = () => {
-  return <div>Login</div>
+  return (
+    <div className="w-full h-screen">
+      <div className="absolute inset-0">
+        <Image
+          src={
+            'https://repository-images.githubusercontent.com/299409710/b42f7780-0fe1-11eb-8460-e459acd20fb4'
+          }
+          alt={'bg'}
+          fill
+        />
+      </div>
+      <div className="relative z-10 w-[500px] h-[50vh] bg-black/50 left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 rounded-md px-8 py-4 flex items-center justify-center">
+        <Button
+          className="mt-4 flex items-center gap-2 w-full bg-red-600 text-white h-[56px] hover:bg-red-400"
+          onClick={() => signIn('github')}
+        >
+          <FaGithub className="w-7 h-7" /> Sign in with github
+        </Button>
+      </div>
+    </div>
+  )
 }
 export default Login
